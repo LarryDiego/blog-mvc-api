b1866ad638bcc3cf1964c54dfea1bd1832a56279
LarryDiego <larry_diego@hotmail.com>
2024-11-17 15:52:35 -0300

    servidor iniciado

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('node:path');
+
+const app = express();
+
+// Configuração do EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Configuração de arquivos estáticos
+app.use(express.static('public'));
+
+// Configuração para ler dados da requisição
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+  console.log(`Servidor iniciado!\nRodando em htpp://localhost:${PORT}/`)
+);
